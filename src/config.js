61591afe12a8d9a9e39609841d58cd5acfcf18ac
Dev <dev34343@gmail.com>
2020-07-01 16:46:28 -0400

    avance

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    baseUrlApp: 'http://127.0.0.1:3000',
-    baseUrlApi: 'http://127.0.0.1:8082',
-    baseUrlAuth : 'http://127.0.0.1:8082/',
-    staticFrontChat : 'http://127.0.0.1:8082/static',
+    baseUrlApp: 'http://app.v1.acyr.cl',
+    baseUrlApi: 'http://api.v1.acyr.cl',
+    baseUrlAuth : 'http://api.v1.acyr.cl/',
+    staticFrontChat : 'http://api.v1.acyr.cl/static',
     client_id: 'bf30c00abc3cb648295dc2b5340861d1',
     'chat_welcome_message_init' : '<p>Please complete the following information to start a conversation.</p>',
     'chat_welcome_message_start' : 'Hello, My name is BELISA and I am a virutal assistant. How I can help?',
