38bb20e195c8bdc87ddcafc59d1255708c04df4e
Dev <dev34343@gmail.com>
2020-07-02 18:23:33 -0400

    cookie

--- src/components/views/chat.js
+++ src/components/views/chat.js
@@ -47,8 +47,8 @@ export default class Login extends Component {
 
   componentDidMount(){
     const client_id = this.props.location.query.i;
-    //if(this._vldParamasGet() == false){
-    if(true == false){
+    if(this._vldParamasGet() == false){
+    //if(true == false){
     }else{
       const cookies = new Cookies();
       if(typeof cookies.get('token') != 'undefined'){
@@ -68,11 +68,11 @@ export default class Login extends Component {
               }}
         ).then(res => {
             const cookies = new Cookies();
-            cookies.set('confChatInit', res.data.data.config[0], {path: '/', sameSite: 'None'});
+            cookies.set('confChatInit', res.data.data.config[0], {path: '/', sameSite: 'lax'});
         }).catch(function (error) {
           //this.bake_cookie('init', false);
           const cookies = new Cookies();
-          cookies.set('confChatInit', false, {path: '/', sameSite: 'None'});
+          cookies.set('confChatInit', false, {path: '/', sameSite: 'lax'});
         }).then(function () {
         });
 
@@ -102,7 +102,7 @@ export default class Login extends Component {
     var oldItems = cookies.get('messages') || [];
     const items = oldItems.slice();
     items.push(item);
-    cookies.set('messages', items, {path: '/', sameSite: 'None'});
+    cookies.set('messages', items, {path: '/', sameSite: 'lax'});
     this.setState({'listMessages' : cookies.get('messages')});
   }
 
@@ -170,8 +170,8 @@ export default class Login extends Component {
              this.setState({showContChat : true});
              /*##*/
              const cookies = new Cookies();
-             cookies.set('token', res.data.data.token, {path: '/', sameSite: 'None'});
-             cookies.set('key_temp', res.data.data.key_temp, {path: '/', sameSite: 'None'});
+             cookies.set('token', res.data.data.token, {path: '/', sameSite: 'lax'});
+             cookies.set('key_temp', res.data.data.key_temp, {path: '/', sameSite: 'lax'});
              if(cookies.get('confChatInit') == false){
               this.setMessage('_res', config.get('chat_welcome_message_start'));
              }else{
@@ -227,8 +227,8 @@ export default class Login extends Component {
 
   render() {
     //add recaptcha..
-    //if(this._vldParamasGet() == false){
-    if(true == false){
+    if(this._vldParamasGet() == false){
+    //if(true == false){
       return ('');
     }else{
         return (
