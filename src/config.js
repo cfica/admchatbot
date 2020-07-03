e80065334ccb66d1773a518a195eccb9658287a7
Dev <dev34343@gmail.com>
2020-07-03 13:11:15 -0400

    domain ssl

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    baseUrlApp: 'http://app.v1.acyr.cl',
-    baseUrlApi: 'http://api.v1.acyr.cl',
-    baseUrlAuth : 'http://api.v1.acyr.cl/',
-    staticFrontChat : 'http://api.v1.acyr.cl/static',
+    baseUrlApp: 'https://app-v1.acyr.cl',
+    baseUrlApi: 'https://api-v1.acyr.cl',
+    baseUrlAuth : 'https://api-v1.acyr.cl',
+    staticFrontChat : 'https://api-v1.acyr.cl/static',
     client_id: 'bf30c00abc3cb648295dc2b5340861d1',
     'chat_welcome_message_init' : '<p>Please complete the following information to start a conversation.</p>',
     'chat_welcome_message_start' : 'Hello, My name is BELISA and I am a virutal assistant. How I can help?',
