18507c2fb4f22d443878023bf3f37a07d9198821
Dev <dev34343@gmail.com>
2020-07-04 20:34:46 -0400

    config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    baseUrlApp: 'https://app.cl',
-    baseUrlApi: 'https://api.cl',
-    baseUrlAuth : 'https://api.cl',
-    staticFrontChat : 'https://api.cl/static',
+    baseUrlApp: 'https://app-v1.acyr.cl.cl',
+    baseUrlApi: 'https://api-v1.acyr.cl',
+    baseUrlAuth : 'https://api-v1.acyr.cl',
+    staticFrontChat : 'https://api-v1.acyr.cl/static',
     client_id: 'bf30c00abc3cb648295dc2b5340861d1',
     'chat_welcome_message_init' : '<p>Please complete the following information to start a conversation.</p>',
     'chat_welcome_message_start' : 'Hello, My name is BELISA and I am a virutal assistant. How I can help?',
