87c7909c18993d5ae87545bf0cf0c18abb0c1aee
Dev <dev34343@gmail.com>
2020-07-14 21:40:25 -0400

    ws api getway

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     baseUrlApp: 'https://app-v1.acyr.cl',
-    baseUrlApi: 'https://api-v1.acyr.cl',
+    baseUrlApi: 'https://ps4v42xh6a.execute-api.us-east-1.amazonaws.com/Test',
     baseUrlAuth : 'https://api-v1.acyr.cl',
     staticFrontChat : 'https://api-v1.acyr.cl/static',
     client_id: 'bf30c00abc3cb648295dc2b5340861d1'
