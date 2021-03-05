e8056742a25f2e7b8cf6655ba9a59d1dc60cc12f
Dev <dev34343@gmail.com>
2021-03-05 00:49:44 -0300

    fix url apps

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    baseUrlApp: 'https://app.cl',
-    baseUrlApi: 'https://api.cl',
-    baseUrlAuth : 'https://api.cl',
-    staticFrontChat : 'https://api.cl/static',
+    baseUrlApp: 'https://app-v1.acyr.cl',
+    baseUrlApi: 'https://api-v1.acyr.cl',
+    baseUrlAuth : 'https://api-v1.acyr.cl',
+    staticFrontChat : 'https://api-v1.acyr.cl/static',
     client_id: 'bf30c00abc3cb648295dc2b5340861d1'
 };
 export default config;
